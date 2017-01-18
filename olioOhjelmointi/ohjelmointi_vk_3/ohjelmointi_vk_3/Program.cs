2b09189769e4b29a608ecf2944eb721b5a8823e5
K8491 <K8491@ICT-421-WS-01>
2017-01-18 15:28:35 +0200

    asdfghj

--- olioOhjelmointi/ohjelmointi_vk_3/ohjelmointi_vk_3/Program.cs
+++ olioOhjelmointi/ohjelmointi_vk_3/ohjelmointi_vk_3/Program.cs
@@ -22,6 +22,11 @@ namespace ohjelmointi.VK3
             Console.WriteLine("Kiuas tehtava (nro 1)");
             Console.WriteLine("Pesukone tehtava (nro 2)");
             Console.WriteLine("Televisio tehtava (nro 3)");
+            Console.WriteLine("Hissi tehtava (nro 8)");
+            Console.WriteLine("vahvistin tehtava (nro 9)");
+            Console.WriteLine("Tietojen käsittely tehtava (nro 10)");
+            Console.WriteLine("Ajoneuvojen käsittely tehtava (nro 11)");
+       
             luku = Convert.ToInt32(Console.ReadLine());
             switch (luku)
             {
@@ -51,6 +56,42 @@ namespace ohjelmointi.VK3
                         break;
                     }
 
+                case 8:
+                    {
+                        Console.Title = "Tehtava 8";
+                        TestaaSaadin();
+                        break;
+                    }
+                case 9:
+                    {
+                        Console.Title = "Tehtava 9";
+                        TestaaVahvistin();
+                        break;
+                    }
+                case 10:
+                    {
+                        Console.Title = "Tehtava 10";
+                        TestaaTietojenKasittely();
+                        break;
+                    }
+                case 11:
+                    {
+                        Console.Title = "Tehtava 11";
+                        TestaaRadio();
+                        break;
+                    }
+                case 12:
+                    {
+                        Console.Title = "Tehtava 12";
+                        TestaaHylly();
+                        break;
+                    }
+                case 13:
+                    {
+                        Console.Title = "Tehtava 13";
+                        //TestaaOhjelmoijanPinnaa();
+                        break;
+                    }
 
             }
         }
@@ -106,5 +147,98 @@ namespace ohjelmointi.VK3
             Console.WriteLine("Televisiossa on aanet ja ne kuuluvat {0} lujaa", televiso.Aani);
 
         }
+
+        static void TestaaSaadin()
+        {
+            //tehtävä 1
+            {
+                /*
+                 * https://msdn.microsoft.com/en-us/library/ms229059(v=vs.110).aspx
+                      *    Tehtävän anti    *
+
+                           Tehtävänäsi on ohjelmoida Dynamon hissin
+                           kerroksen ohjaukseen liittyvä säädin.
+
+                           Toteutetun ohjelman tulee pystyä kysymään 
+                           käyttäjältä haluttu kerros ja siirtämään hissi
+                           haluttuun kerrokseen (tässä tapauksessa ohjelma
+                           kertoo käyttäjälle missä kerroksessa hissi on).
+
+                           Muista, että Dynamon hissi voi olla vain kerroksissa
+                           1-5. Käytä Hissi-luokassa get- ja set-aksessoreita 
+                           suojamaan olion tila.
+
+
+                    ESIMERKKI         *             ESIMERKKI
+                    Elevator is now in floor : 1
+                    Give a new floor number (1-5) > 2
+                    ESIMERKKI         *             ESIMERKKI
+                */
+                Saadin saadin = new Saadin();
+                saadin.OnkoPaalla = true;
+                saadin.Kerros = 0;
+                int number = 0;
+                string line;
+
+                Console.Write("Hissi Ohjelma\n x/X lopettaa ohjelman\n (ctrl + c panikoiville)\n");
+                do
+                {
+                  
+                    // ask number from the user (read one line)
+                 Console.Write("Hissi on kerroksessa {0} > ", saadin.Kerros);
+                   line = Console.ReadLine();
+                    bool result = int.TryParse(line, out number);
+                    Console.Clear();
+                    // number (integer) was given correctly, use it..
+                    if (result)
+                    {
+                        Console.WriteLine("Number was " + number);
+                        if(!(number<0 || number >5))
+                        saadin.Kerros = number;
+                        else
+                        {
+                            Console.WriteLine("Rakennukseesa on vain kerrokset 1-5, kokeile uudestaan");
+                        }
+
+                        Console.WriteLine("floor:  " + saadin.Kerros);
+                        if (!(saadin.Kerros == number))
+                        {
+
+                            Console.WriteLine("Annoitko kelvollisen numeron?? ");
+                            Console.WriteLine("Liikumme kerrokseen "+saadin.Kerros);
+                            Console.WriteLine("Kokeile hetken kuluttua uudelleen ");
+                        }
+                    }
+                } while (!(line == "x" || line == "X"));
+                
+                }
+        }
+        static void TestaaVahvistin()
+        {
+            // tehtävä 2
+
+        }
+        static void TestaaTietojenKasittely()
+        {
+            // tehtävä 3
+
+        }
+        static void TestaaAjoneuvojenKasittely()
+        {
+            // tehtävä 4
+
+        }
+        static void TestaaHylly()
+        {
+            // tehtävä 6
+
+        }
+        static void TestaaRadio()
+        {
+            // tehtävä 5
+
+        }
+        //muista tehdä 7
+
     }
 }
