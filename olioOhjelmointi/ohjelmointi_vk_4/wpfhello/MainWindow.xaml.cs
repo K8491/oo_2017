0dfb60e9743824058cbdb8fbbc9430ec6af33b2a
K8491 <K8491@ICT-421-WS-01>
2017-02-07 23:13:50 +0200

    homeworkz

--- olioOhjelmointi/ohjelmointi_vk_4/wpfhello/MainWindow.xaml.cs
+++ olioOhjelmointi/ohjelmointi_vk_4/wpfhello/MainWindow.xaml.cs
@@ -32,14 +32,94 @@ namespace wpfhello
         {
          //  textBlock.Text = "Hello " + textBox.Text;
           // MessageBox.Show("Terve "+ textBox.Text, "Antti's Duu duu");
-            textBlock1.Text = " " + (++t) + " " + (i);
+            textBlock2.Text = " " + (++t) + " " + (i);
         }
         private void button1_Copy_Click(object sender, RoutedEventArgs e)
         {
             // kutsutaan nakyviin Tiedoksi ikkuna
             //About tiedoksiUusi = new About();
             // tiedoksiUusi.ShowDialog();
-            textBlock1.Text = " " + (t) + " " + (++i);
+            textBlock2.Text = " " + (t) + " " + (++i);
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double number = 0;
+                string value = textBox2.Text;
+                double.TryParse(value, out number);
+                number = number * 6;
+                value = number.ToString("0.00");
+                textBox2.Text = value;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+          
+        }
+
+        private void button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                double number = 0;
+                string value = textBox2.Text;
+                double.TryParse(value, out number);
+                number = number / 6;
+                value = number.ToString("0.00");
+                textBox2.Text = value;
+               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //ikkuna textBox3 textBlock3
+                //lasi textBox3_1 textBlock3_1
+                //karmi textBox3_2 textBlock3_2
+
+                int number = 0;
+                int number1 = 0;
+                int number2 = 0;
+                // otetaan talteen
+                string value = textBox3.Text;
+                int.TryParse(value, out number);
+
+                value = textBox3_1.Text;
+                int.TryParse(value, out number1);
+
+                value = textBox3_2.Text;
+                int.TryParse(value, out number2);
+
+                //lasketaan arvoilla
+                // a*b
+                textBlock3.Text = ((number*number1)/10).ToString("0");
+                // pituus ja levys suunnassa poistetaan karmin paksuuden verran
+                //(ylhaalta ja alhaalta, oikealta ja vasemalta on a-x * b-x... siis pitais olla
+                textBlock3_1.Text = (((number-(number2*2)) * (number1-(number2)*2)) / 10).ToString("0");
+                //pituus kertaa pituus plus leveys kertaa leveys
+                textBlock3_2.Text = ((number*2 + number1*2 )/10).ToString("0");
+
+                //lisataan tekstia merkkijonoon
+                textBlock3.Text = textBlock3.Text + " cm^2";
+                textBlock3_1.Text = textBlock3_1.Text + " cm^2";
+                textBlock3_2.Text = textBlock3_2.Text + " cm";
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
