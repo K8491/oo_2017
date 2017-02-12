615e344375554dcce1c196aafb5882d4c8109997
K8491 <K8491@ICT-421-WS-01>
2017-02-12 20:45:07 +0200

    paskaa koodia

---
+++ olioOhjelmointi/ohjelmointi_vk_4/KT2/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KT
+{
+    /// <summary>
+    /// Interaction logic for KT2.xaml
+    /// </summary>
+    public partial class KT2 : Window
+    {
+        public KT2()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+               Debug.WriteLine(" " + textBox.Text + ": " + textBox1.Text);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+           
+
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
