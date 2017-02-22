e76b9d8e42b83e532bb2d5918ddf7ba6693e9ed8
K8491 <K8491@ICT-421-WS-01>
2017-02-22 11:34:51 +0200

    wasedfgh

---
+++ Jotain/MVVMDemo/ViewModel/StudentViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using MVVMDemo.Model;
+
+namespace MVVMDemo.ViewModel
+{
+ public class StudentViewModel
+    {
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+       public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Model.Student>();
+            // lisataan esimerkin vuoksi muutama opiskelija. oikeassa sovelluksessa haettaisiin tietokannasta
+            students.Add(new Student { FirstName = "Kalle", LastName = "Jalkanen" , Asioid="K2392"});
+            students.Add(new Student { FirstName = "Teppo", LastName = "Testaaja", Asioid="H2652"});
+            students.Add(new Student { FirstName = "Tomi", LastName = "Tottestrom", Asioid = "T3242" });
+            students.Add(new Student { FirstName = "Tuomas", LastName = "Tuovaunen", Asioid="I1235" });
+            Students = students;
+        }
+    }
+}
